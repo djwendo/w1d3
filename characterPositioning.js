f5811decee0f3eb9b5f50ab769e84d80ca8cab03
Wendy Braun <wendocalrissian@gmail.com>
2018-03-14 21:26:36 +0000

    Adding characterPosition.js

--- characterPositioning.js
+++ characterPositioning.js
@@ -3,15 +3,14 @@ var str = "lighthouse in the house";
 
 function countLetters(str) {
   var arr = str.split("");
-  console.log(arr);
   var result = {};
 
   for (var i = 0; i < arr.length; i++) {
     if (arr[i] !== " ") {
-      result[''+arr[i]] = 0;
+      result[arr[i]] = [];
     for (var j = 0; j < arr.length; j++) {
-      if (''+arr[i] === ''+arr[j]) {
-        result[''+arr[j]] = result[''+arr[j]] + 1;
+      if (arr[i] === arr[j]) {
+        result[arr[i]].push(j);
         }
       }
     }
